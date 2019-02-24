4873315a442797f4a451ed40d2df445373417e58
EsperantoDeparture <juanpablordmsx2@gmail.com>
2019-02-24 16:21:23 -0500

    Go to graph button

--- EfectoCompton/src/app/home/home.component.ts
+++ EfectoCompton/src/app/home/home.component.ts
@@ -1,15 +1,36 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  animate,
+  state,
+  style,
+  transition,
+  trigger
+} from '@angular/animations';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
+  animations: [
+    trigger('fadeInOut', [
+      state(
+        'void',
+        style({
+          opacity: 0,
+          height: '0px',
+          display: 'none'
+        })
+      ),
+      transition('void <=> *', animate(500))
+    ])
+  ]
 })
 export class HomeComponent implements OnInit {
   angles: number[] = [];
   newAngle: number;
 
-  constructor() {}
+  constructor(private router: Router) {}
 
   ngOnInit() {}
 
@@ -17,11 +38,26 @@ export class HomeComponent implements OnInit {
     if (this.newAngle === undefined) {
       return;
     }
+    if (this.angles.indexOf(this.newAngle) !== -1) {
+      return;
+    }
     this.angles.push(this.newAngle);
+    this.angles.sort();
     this.newAngle = undefined;
   }
 
   remove(val: number) {
     this.angles.splice(this.angles.findIndex(angle => angle === val), 1);
+  }
+
+  graph() {
+    if (!this.angles.length) {
+      return;
+    }
+    this.router.navigate(['grafica'], {
+      queryParams: {
+        angles: this.angles.join()
+      }
+    });
   }
 }
