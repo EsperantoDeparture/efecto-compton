6bbb372797195b7a7181f9d5384bebc589c4a16c
EsperantoDeparture <juanpablordmsx2@gmail.com>
2019-02-24 16:10:08 -0500

    More material modules

--- EfectoCompton/src/app/app.module.ts
+++ EfectoCompton/src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { BrowserModule } from '@angular/platform-browser';
+import { BrowserModule, DomSanitizer } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -6,10 +6,19 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { GraphComponent } from './graph/graph.component';
 import { HomeComponent } from './home/home.component';
-import { MatButtonModule, MatInputModule } from '@angular/material';
+import {
+  MatButtonModule,
+  MatInputModule,
+  MatListModule,
+  MatDividerModule,
+  MatIconModule,
+  MatIconRegistry,
+  MatTooltipModule
+} from '@angular/material';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { NgxDygraphsModule } from 'ngx-dygraphs';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [AppComponent, HeaderComponent, GraphComponent, HomeComponent],
@@ -19,11 +28,27 @@ import { NgxDygraphsModule } from 'ngx-dygraphs';
     BrowserAnimationsModule,
     FormsModule,
     ReactiveFormsModule,
+    MatIconModule,
     MatButtonModule,
     MatInputModule,
-    NgxDygraphsModule
+    NgxDygraphsModule,
+    MatListModule,
+    MatDividerModule,
+    MatTooltipModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [HttpClientModule],
   bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule {
+  constructor(iconRegistry: MatIconRegistry, sanitizer: DomSanitizer) {
+    iconRegistry.addSvgIcon(
+      'add',
+      sanitizer.bypassSecurityTrustResourceUrl('assets/add.svg')
+    );
+    iconRegistry.addSvgIcon(
+      'remove',
+      sanitizer.bypassSecurityTrustResourceUrl('assets/remove.svg')
+    );
+  }
+}
