dcbbb954d5465fca37c4871855d9e9e2e093a0c0
EsperantoDeparture <juanpablordmsx2@gmail.com>
2019-02-24 16:09:47 -0500

    Routes

--- EfectoCompton/src/app/app-routing.module.ts
+++ EfectoCompton/src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { GraphComponent } from './graph/graph.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'grafica', component: GraphComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
