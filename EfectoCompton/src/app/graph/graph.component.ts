474b6973ac677238f2f0e0c306e8107477974849
EsperantoDeparture <juanpablordmsx2@gmail.com>
2019-02-25 07:01:32 -0500

    Graph

--- EfectoCompton/src/app/graph/graph.component.ts
+++ EfectoCompton/src/app/graph/graph.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-graph',
@@ -6,10 +7,51 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./graph.component.scss']
 })
 export class GraphComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+  data: string;
+  h = 6.62607004 * Math.pow(10, -34);
+  c = 3 * Math.pow(10, 8);
+  me = 9.10938356 * Math.pow(10, -31);
+  options: any = {
+    pointSize: 3,
+    highlightCircleSize: 2.5,
+    drawPoints: true,
+    strokeWidth: 0.0,
+    displayAnnotations: true,
+    width: '100%',
+    height: 250,
+    legend: 'always',
+    axes: {
+      y: {
+        valueFormatter: (v: number) => {
+          return `${v.toExponential(4)}m`; // controls formatting in the legend/mouseover
+        },
+        axisLabelFormatter: (v: number) => {
+          return `${v.toExponential(2)}m`; // controls formatting of the y-axis labels
+        }
+      },
+      x: {
+        valueFormatter: (v: number) => {
+          return `${v}°`; // controls formatting in the legend/mouseover
+        },
+        axisLabelFormatter: (v: number) => {
+          return `${v}°`; // controls formatting of the y-axis labels
+        }
+      }
+    }
+  };
+  comptonWaveLength = this.h / (this.me * this.c);
+  constructor(private activatedRoute: ActivatedRoute) {
+    this.activatedRoute.queryParams.subscribe(params => {
+      const angles: number[] = params['angles'].split(',');
+      let aux = 'Angulo, \u03BB\'\n';
+      const waveLength = parseFloat(params['waveLength']);
+      for (const angle of angles) {
+        aux += `${angle}, ${waveLength +
+          this.comptonWaveLength * (1 - Math.cos(angle))}\n`;
+      }
+      this.data = aux;
+    });
   }
 
+  ngOnInit() {}
 }
