34a8dec145081396dd6c5237494ccfcd31585748
EsperantoDeparture <juanpablordmsx2@gmail.com>
2019-02-25 12:40:31 -0500

    Draw a table

--- EfectoCompton/src/app/graph/graph.component.ts
+++ EfectoCompton/src/app/graph/graph.component.ts
@@ -7,13 +7,14 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./graph.component.scss']
 })
 export class GraphComponent implements OnInit {
-  data: string;
+  csv: string;
   h = 6.62607004 * Math.pow(10, -34);
   c = 3 * Math.pow(10, 8);
   me = 9.10938356 * Math.pow(10, -31);
+  dataTable: number[][] = [];
   options: any = {
     pointSize: 3,
-    highlightCircleSize: 2.5,
+    highlightCircleSize: 5,
     drawPoints: true,
     strokeWidth: 0.0,
     displayAnnotations: true,
@@ -37,19 +38,32 @@ export class GraphComponent implements OnInit {
           return `${v}°`; // controls formatting of the y-axis labels
         }
       }
-    }
+    },
+    dateWindow: [-10, 370]
   };
   comptonWaveLength = this.h / (this.me * this.c);
   constructor(private activatedRoute: ActivatedRoute) {
     this.activatedRoute.queryParams.subscribe(params => {
-      const angles: number[] = params['angles'].split(',');
+      const angles: number[] = params['angles']
+        .split(',')
+        .map((angle: string) => parseFloat(angle));
+      if (!angles || !angles.length) {
+        return;
+      }
       let aux = 'Angulo, \u03BB\'\n';
       const waveLength = parseFloat(params['waveLength']);
       for (const angle of angles) {
-        aux += `${angle}, ${waveLength +
-          this.comptonWaveLength * (1 - Math.cos(angle))}\n`;
+        const scatteredWavelength: number =
+          waveLength +
+          this.comptonWaveLength * (1 - Math.cos(0.0174532925 * angle));
+        this.dataTable.push([angle, scatteredWavelength]);
+        aux += `${angle}, ${scatteredWavelength}\n`;
       }
-      this.data = aux;
+      this.options.dateWindow[0] = angles[0] - 10;
+      this.options.dateWindow[1] = angles[angles.length - 1] + 10;
+      console.log(angles);
+      console.log(this.options.dateWindow);
+      this.csv = aux;
     });
   }
 
